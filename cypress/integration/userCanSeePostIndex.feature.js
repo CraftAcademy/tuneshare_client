1c236390caa91d8ceb0f3738ca584b54f99d78cd
viktor <viktorkarlsson125@gmail.com>
2021-01-25 20:29:46 +0100

    creating a fixture with post from backend. writing first test. creating postIndex component

---
+++ cypress/integration/userCanSeePostIndex.feature.js
@@ -0,0 +1,30 @@
+describe("User can see a index list of posts", () => {
+  beforeEach(() => {
+    cy.server()
+    cy.intercept(
+      "http://localhost:3000/api", 
+      {
+        fixture: "fixture:postIndex.json"
+      }
+    )
+    // cy.route({
+    //   method: "GET",
+    //   url:  "localhost:3000/api",
+    //   response: "fixture:postIndex.json"
+    // })
+    cy.visit("/")
+  })
+
+  describe("successfully", () => {
+    it("If the server is runnig", () => {
+      cy.get("[data-cy='index']").within(() => {
+        cy.get("[data-cy='track_data']").should("contain", "All I Want for Christmas Is You", "Mariah Carey")
+        cy.get("[data-cy='description']").should("contain", "This is the best christmas song ever! All time favorite!")
+        cy.get("[data-cy='track_data']").should("contain", "Last Christmas", "Wham!")
+        cy.get("[data-cy='description']").should("contain", "Please don't say you don't know this song! Very classic christmas song.")
+        cy.get("[data-cy='track_data']").should("contain", "It's Beginning to Look a Lot like Christmas", "Michael Bublé")
+        cy.get("[data-cy='description']").should("contain", "Everyone can agree on this makes christmas perfect!")
+      })
+    })
+  })
+})
