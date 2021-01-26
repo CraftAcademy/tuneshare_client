a05d75f84309f50a0e2d31385fde94b0ada5d378
sealfury <willspecker@gmail.com>
2021-01-26 22:05:43 +0100

    adds expo-av package creates functionality to play song preview for each track in index

--- src/components/postIndex.jsx
+++ src/components/postIndex.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Text, Button } from 'react-native'
 import { Card } from 'react-native-elements'
+import { Audio } from 'expo-av'
 import styles from '../styles/styles'
 
-const PostIndex = ({post}) => {
+const PostIndex = ({ post }) => {
+  const [song, setSong] = useState()
+  const playSong = async () => {
+    const { song } = await Audio.Sound.createAsync(
+      { uri: post.preview_url },
+      { shouldPlay: true }
+    )
+    debugger
+    setSong(song)
+
+    await song.playAsync()
+  }
+  useEffect(() => {
+    return song
+      ? () => {
+          song.unloadAsync()
+        }
+      : undefined
+  }, [song])
   return (
     <Card style={styles.card_container}>
       <Card.Title style={styles.track}>{post.track}</Card.Title>
       <Card.Title style={styles.artists}>{post.artists}</Card.Title>
       <Card.Divider />
-      <Card.Image style={styles.image} resizeMode="contain" source={{ uri: post.image }} >
-      </Card.Image>
+      <Card.Image
+        style={styles.image}
+        resizeMode='contain'
+        source={{ uri: post.image }}
+      ></Card.Image>
       <Card.Divider />
-        <Text style={styles.description}>
-          {post.description}
-        </Text>
+      <Text style={styles.description}>{post.description}</Text>
+      <Button title='Play a preview!' onPress={() => playSong} />
     </Card>
   )
 }
