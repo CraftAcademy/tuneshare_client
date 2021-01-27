3e0cd5d2327f3f63b815143e254c6646347faa1a
Kyungin Na <nakyungin93@gmail.com>
2021-01-27 10:49:56 +0100

    Creating PostForm component Adding Tab navigator in App component

--- App.jsx
+++ App.jsx
@@ -4,20 +4,35 @@ import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './src/components/HomeScreen'
 import { useSelector } from 'react-redux'
 
+import PostForm from './src/components/PostForm'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 const App = () => {
   const { appTitle } = useSelector(state => state)
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="HomeScreen"
-          component={HomeScreen}
-          options={() => ({ title: appTitle })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      {/* <Appbar.Header>
+        <Appbar.Content title={appTitle} />
+      </Appbar.Header> */}
+      {/* <NavigationContainer> */}
+      {/* </NavigationContainer> */}
+          {/* <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={() => ({ title: appTitle })}
+            />
+          </Stack.Navigator> */}
+      <NavigationContainer style={{ height: 10 }}>
+        <Tab.Navigator>
+          <Tab.Screen name="Feed" component={HomeScreen} />
+          <Tab.Screen name="Post" component={PostForm} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   )
 }
 
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const PostForm = () => {
+  return (
+    <View>
+      <Text>
+        This is postform!
+      </Text>
+    </View>
+  )
+}
+
+export default PostForm
+
+const styles = StyleSheet.create({})
