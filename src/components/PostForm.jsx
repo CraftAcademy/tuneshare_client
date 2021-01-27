6fb8a41130ea3a941061aae5a00cda456124c71c
Sanne <sannelindholm@hotmail.com>
2021-01-27 11:15:28 +0100

    Adding onChangeText to searchbar with value and adding usestate

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 import { SearchBar } from 'react-native-elements'
 
 const PostForm = () => {
+  const [search, setSearch] = useState()
+
   return (
     <View>
       <SearchBar
-      placeholder="Search here..."/>
+      placeholder="Search here..."
+      onChangeText={(text) => setSearch(text)}
+      value={search}
+      />
       <Text>
        New post!
       </Text>
