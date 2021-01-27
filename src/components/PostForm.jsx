531f7ff3356f7bc5d5a3d6f7d7d383713a51b37d
Rithreaksa <wonderfulreaksa@gmail.com>
2021-01-27 13:33:12 +0100

    WIP- fectching data from API to display in search function

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,25 +1,44 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
-import { SearchBar } from 'react-native-elements'
+import { ListItem, SearchBar } from 'react-native-elements'
+import { FlatList } from 'react-native-gesture-handler'
+import TrackService from '../modules/TrackService'
+import { useSelector } from 'react-redux'
 
 const PostForm = () => {
-  const [search, setSearch] = useState()
+  //const [search, setSearch] = useState()
+
+  const { searchResult } = useSelector(state => state)
+
+  // useEffect(() => {
+  //   TrackService.index(index)
+  // }, [])
 
   return (
     <View>
       <SearchBar
-      placeholder="Search here..."
-      onChangeText={(text) => setSearch(text)}
-      value={search}
+        placeholder='Search here...'
+        onChangeText={text => {
+          TrackService.index(text)
+          //setSearch(text)
+        }}
+        //value={search}
       />
-      <Text>
-       New post!
-      </Text>
-      <TextInput
-      placeholder="Text"
+      <Text>New post!</Text>
+      <TextInput placeholder='Text' />
+      <Button title='Submit' />
+      <FlatList
+        data={searchResult}
+        renderItem={({ item }) => (
+          <ListItem
+            track={item.track}
+            artitsts={item.artitsts}
+            image={item.image}
+            preview={item.preview}
+          />
+        )}
+        keyExtractor={item => item.preview}
       />
-      <Button
-      title="Submit"/>
     </View>
   )
 }
--- src/modules/TrackService.js
+++ src/modules/TrackService.js
@@ -4,8 +4,16 @@ import store from '../state/store/store'
 const API_URL = 'http://localhost:3000/api'
 
 const TrackService = {
-  async index() {
-    let response = await axios.get(`${API_URL}/tracks`)
+  async index(text) {
+   
+    let response = await axios.get(`${API_URL}/tracks`, {
+   
+      params: {
+        q: text,
+      },
+     
+    })
+    
     store.dispatch({ type: 'SET_SEARCH_INDEX', payload: response.data })
   },
 }
