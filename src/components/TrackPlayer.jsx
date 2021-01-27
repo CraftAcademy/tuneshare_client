02257c2d4fb09a0a31f5f4aedcde3ce1c658b5e7
sealfury <willspecker@gmail.com>
2021-01-27 14:11:31 +0100

    finalizes functionality for start and stop playback of all songs in index adds ternary rendering of play button based on playback status moves playback button above post description field

--- src/components/TrackPlayer.jsx
+++ src/components/TrackPlayer.jsx
@@ -1,49 +1,35 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import { Button, View } from 'react-native'
 import { Audio } from 'expo-av'
+import styles from '../styles/styles'
+
 
 const TrackPlayer = ({post}) => {
-  const [playSong, setPlaySong] = useState()
-  const [stopSong, setStopSong] = useState()
+  let songURI = post.preview_url
+  const [playback, setPlayback] = useState()
 
-  const startPlayback = async () => {
-    const { playSong } = await Audio.Sound.createAsync(
-      { uri: post.preview_url },
-      { shouldPlay: true }
-    )
-    setPlaySong(playSong) 
+
+  async function startPlayback() {
+    const playback = new Audio.Sound()
+    await playback.loadAsync({uri: songURI})
+    await playback.playAsync()
+    setPlayback(playback)
   }
-
-  const stopPlayback = async () => {
-    const {isPlaying} = await startPlayback(playSong)
-    const {stopSong} = isPlaying.stopAsync()
-    setStopSong(stopSong)
+  async function stopPlayback() {
+    await playback.stopAsync()
+    setPlayback(undefined)
   }
-
-  // useEffect(() => {
-  //   return song
-  //     ? () => {
-  //         song.stopAsync()
-  //       }
-  //     : undefined
-  // }, [song])
 
   return (
-    <View>
-      <Button title='Play a preview!' onPress={startPlayback} />
-      <Button title='Stop preview!' onPress={stopPlayback} />
-    </View>
+    <View style={styles.card_container}>
+      <Button
+        id={`post${post.id}`}
+        color={ playback ? 'red' : 'blue'} 
+        title={ playback ? "Stop Playback" : 'Start Playback' }
+        onPress={ playback ? stopPlayback : startPlayback }
+      />
+      </View>
   )
 }
 
 export default TrackPlayer
-
-  // const stopPlayback = async () => {
-  //   let playSong = {playSong}
-  //   const {isPlaying} = await playSong.loadAsync(
-  //     { uri: post.preview_url },
-  //     { initialStatus: playSong }
-  //   )
-  //   const {stopSong} = isPlaying.stopAsync()
-  //   setStopSong(stopSong)
-  // }
--- src/components/postIndex.jsx
+++ src/components/postIndex.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import {  View, Text } from 'react-native'
 import { Card } from 'react-native-elements'
 import styles from '../styles/styles'
 import TrackPlayer from './TrackPlayer'
 
 const PostIndex = ({ post }) => {
-
-  // const trackPreview = (e) => {
-  //   e.preventDefault()
-  //   playSong(e, post)
-  // }
-
   return (
     <View style={styles.card_container}>
-    <Card>
-      <Card.Title style={styles.track}>{post.track}</Card.Title>
-      <Card.Title style={styles.artists}>{post.artists}</Card.Title>
-      <Card.Divider />
-      <Card.Image
-        style={styles.image}
-        resizeMode='contain'
-        source={{ uri: post.image }}
-      ></Card.Image>
-      <Card.Divider />
-      <Text style={styles.description}>{post.description}</Text>
-    </Card>
-    <TrackPlayer post={post}/>
+      <Card>
+        <Card.Title style={styles.track}>{post.track}</Card.Title>
+        <Card.Title style={styles.artists}>{post.artists}</Card.Title>
+        <Card.Divider />
+        <Card.Image
+          style={styles.image}
+          resizeMode='contain'
+          source={{ uri: post.image }}
+        ></Card.Image>
+        <TrackPlayer post={post} />
+        <Card.Divider />
+        <Text style={styles.description}>{post.description}</Text>
+      </Card>
     </View>
   )
 }
