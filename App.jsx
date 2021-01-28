7077ce13b196f495adbf0d6a286a41c1a98219ff
Sanne <sannelindholm@hotmail.com>
2021-01-28 11:31:35 +0100

    Refactoring app component and modifying styling

--- App.jsx
+++ App.jsx
@@ -4,46 +4,39 @@ import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './src/components/HomeScreen'
 import { useSelector } from 'react-redux'
 import styles from './src/styles/styles'
-
 import PostForm from './src/components/PostForm'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
 
+const HomeStack = () => {
+  const { appTitle } = useSelector((state) => state)
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='HomeScreen'
+        component={HomeScreen}
+        options={() => ({
+          title: appTitle,
+          headerStyle: styles.mainHeader,
+          headerTitleStyle: styles.appTitle,
+        })}
+      />
+    </Stack.Navigator>
+  )
+}
+
 const App = () => {
-  const { appTitle } = useSelector(state => state)
   return (
     <>
-      {/* <NavigationContainer> */}
-      {/* </NavigationContainer> */}
-          {/* <Stack.Navigator>
-            <Stack.Screen
-              name="HomeScreen"
-              component={HomeScreen}
-              options={() => ({ title: appTitle })}
-            />
-          </Stack.Navigator> */}
       <NavigationContainer style={{ height: 10 }}>
         <Tab.Navigator>
-          <Tab.Screen name="Feed" component={HomeScreen} />
-          <Tab.Screen name="Post" component={PostForm} />
+          <Tab.Screen name='Feed' component={HomeStack} />
+          <Tab.Screen name='Post' component={PostForm} />
         </Tab.Navigator>
       </NavigationContainer>
     </>
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen
-    //       name='HomeScreen'
-    //       component={HomeScreen}
-    //       options={() => ({
-    //         title: appTitle,
-    //         headerStyle: styles.mainHeader,
-    //         headerTitleStyle: styles.appTitle,
-    //       })}
-    //     />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
   )
 }
 
