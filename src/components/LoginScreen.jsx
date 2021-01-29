f145f9cd5db40fb79b16e1d215248b1a805b3d65
sealfury <willspecker@gmail.com>
2021-01-29 23:14:08 +0100

    configures message and email hooks and imports Auth module

---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { View, TextInput, Text, Pressable } from 'react-native'
+import Auth from '../modules/AuthDevise'
+import { showMessage } from 'react-native-flash-message'
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const deviseAuth = new Auth({
+    host: 'http://localhost:3000/api'
+  })
+
+  
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({})
