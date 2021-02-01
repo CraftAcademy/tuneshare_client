3cdc082058ae068a9576c75fd18b5f5191ce6dd3
sealfury <willspecker@gmail.com>
2021-02-01 11:17:32 +0100

    one last formatting change

--- src/components/PostIndex.jsx
+++ src/components/PostIndex.jsx
@@ -7,15 +7,21 @@ import TrackPlayer from './TrackPlayer'
 const PostIndex = ({ post }) => {
   return (
     <View style={styles.card_container}>
-      <Card testID="post-index">
-        <Card.Title testID={`post-title-${post.id}`} style={styles.track}>{post.track}</Card.Title>
-        <Card.Title testID={`post-artist-${post.id}`} style={styles.artists}>{post.artists}</Card.Title>
+      <Card testID='post-index'>
+        <Card.Title testID={`post-title-${post.id}`} style={styles.track}>
+          {post.track}
+        </Card.Title>
+        <Card.Title testID={`post-artist-${post.id}`} style={styles.artists}>
+          {post.artists}
+        </Card.Title>
         <Card.Divider />
         <Card.Image style={styles.image} source={{ uri: post.image }}>
           <TrackPlayer post={post} />
         </Card.Image>
         <Card.Divider />
-        <Text testID={`post-description-${post.id}`} style={styles.description}>{post.description}</Text>
+        <Text testID={`post-description-${post.id}`} style={styles.description}>
+          {post.description}
+        </Text>
       </Card>
     </View>
   )
