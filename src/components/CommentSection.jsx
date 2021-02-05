28e5a71029630813dfe73ea3164b237c3201d3a6
sealfury <willspecker@gmail.com>
2021-02-05 20:39:24 +0100

    adding shape and colour to comment bubbles

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -19,7 +19,6 @@ const CommentSection = () => {
   const post = route.params.post
   const { comments, credentials } = useSelector(state => state)
   const [newComment, setNewComment] = useState('')
-  let colors = ['#123456', '#654321', '#fdecba', '#abcdef']
 
   useEffect(() => {
     Comments.index(post.id)
@@ -35,20 +34,21 @@ const CommentSection = () => {
   const SingleComment = ({ content }) => (
     <Text
       style={{
-        maxWidth: 200,
-        padding: 10,
+        maxWidth: 400,
+        padding: 8,
+        paddingLeft: 12,
         borderRadius: 20,
         alignSelf: 'flex-start',
+        fontFamily: 'GillSans-SemiBold',
+        color: '#defaeb',
+        textShadowColor: '#161716',
+        textShadowRadius: 1,
       }}
     >
       {content}
     </Text>
   )
-  const renderComment = ({ item }) => (
-   
-      <SingleComment content={item.content} />
-  
-  )
+  const renderComment = ({ item }) => <SingleComment content={item.content} />
 
   return (
     <ScrollView
@@ -63,12 +63,16 @@ const CommentSection = () => {
             margin: 8,
             flex: 1,
             flexDirection: 'row',
-            backgroundColor: colors[index % colors.length],
+            backgroundColor: '#222152',
             borderRadius: 300,
-            padding: 5,
+            borderColor: '#5a58b8',
+            borderBottomWidth: 3,
+            borderRightWidth: 2,
+            borderLeftWidth: 4,
+            padding: 10,
           }}
           data={comments}
-          keyExtractor={(item, index) => index}
+          keyExtractor={item => item.id.toString()}
           renderItem={renderComment}
         />
         <View
