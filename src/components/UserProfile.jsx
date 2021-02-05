ff7e5bd869ce805c9799c9d004f8a508b5bca366
Sanne <sannelindholm@hotmail.com>
2021-02-05 20:14:05 +0100

    Adding stylecheet to component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { Text, View, Dimensions, TouchableOpacity, Image } from 'react-native'
+import { Text, View, Dimensions, TouchableOpacity, Image, StyleSheet } from 'react-native'
 import User from '../modules/UserService'
 import { Avatar, ListItem } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
 import PostService from '../modules/PostService'
 import { Ionicons } from '@expo/vector-icons'
-import styles from '../styles/styles'
+// import styles from '../styles/styles'
 
 const UserProfile = () => {
   const navigation = useNavigation()
@@ -24,12 +24,11 @@ const UserProfile = () => {
 
   return (
     <View>
-      <Text style={styles.container} testID='user-email'>
-      </Text>
+      <Text style={styles.container} testID='user-email'></Text>
       <View style={styles.header}></View>
       <Image
         style={styles.avatar}
-        source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }}
+        source={{ uri: 'https://wallpapercave.com/wp/KwS4poN.jpg' }}
       />
       <View style={styles.body}>
         <View style={styles.bodyContent}>
@@ -52,8 +51,8 @@ const UserProfile = () => {
                   </ListItem.Content>
                   <Ionicons
                     name='trash'
-                    size={30}
-                    color='red'
+                    size={26}
+                    color='#22272c'
                     testID={`delete-button-${item.id}`}
                     onPress={() => {
                       PostService.delete(item.id)
@@ -67,5 +66,37 @@ const UserProfile = () => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#2a4158',
+    height: 120,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: 'white',
+    marginBottom: 10,
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 50,
+  },
+  body: {
+    marginTop: 40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 30,
+  },
+  name: {
+    fontSize: 22,
+    color: '#696969',
+    fontWeight: '600',
+    fontFamily: 'Montserrat'
+  }
+})
 
 export default UserProfile
